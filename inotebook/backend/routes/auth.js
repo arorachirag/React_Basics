9821f931b7c1b13540d65709556fadf1eb2cecd9
arorachirag <arorachirag52@gmail.com>
2022-12-30 23:40:56 +0530

    d

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-router.post('/',(req,res)=>{
+router.post('/',[
+    body('name').isLength({min:3}),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 })
+    
+],(req,res)=>{
     console.log(req.body);
-    res.send('Hello1');
-    const users = User(req.body);
-    users.save();
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user));
+    res.send(req.body);
 })
 
 module.exports = router;
